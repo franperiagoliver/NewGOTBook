853161f07274cb38596ed4f535d7dcfaaf896d54
franperiagoliver <franperiagoliver@gmail.com>
2017-09-16 14:32:04 +0200

    Partner-list

--- new-gotbook/src/app/partner-home/partner-list/partner-list.component.ts
+++ new-gotbook/src/app/partner-home/partner-list/partner-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RelationService } from './relation.service';
+import { Relation } from './relation.model';
+import { Character } from './character/character.model';
+import { CharacterService } from './character/character.service';
 
 @Component({
   selector: 'app-partner-list',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartnerListComponent implements OnInit {
 
-  constructor() { }
+  relations: Relation[];
+  character: Character;
+
+  constructor(private relationService: RelationService, private characterService: CharacterService) { }
 
   ngOnInit() {
+    this.characterService.getCharacter().subscribe(character => this.character = character);
+    this.relationService.getRelation().subscribe(relations => this.relations = relations);
+  }
+
+  getPartnerPicture() {
+    this.relations.forEach(relation => {
+      return '../../../assets/characts/' + relation.requested.picture;
+    });
   }
 
 }
--- new-gotbook/src/app/partner-home/partner-list/relation.service.ts
+++ new-gotbook/src/app/partner-home/partner-list/relation.service.ts
@@ -6,11 +6,11 @@ import { Relation } from './relation.model';
 @Injectable()
 export class RelationService {
 
-  private URLBase = 'http://localhost:3000/';
+  private URLBase = 'http://localhost:3000';
   constructor(private http: Http) { }
 
-  getRelation(relation: Relation): Observable<Relation[]> {
-    return this.http.get(`${this.URLBase}/character/${relation['id']}/relate`)
+  getRelation(): Observable<Relation[]> {
+    return this.http.get(`${this.URLBase}/relation/?requester.id=0`)
         .map((res: Response) => res.json())
         .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
 
@@ -19,7 +19,7 @@ export class RelationService {
   updateRelation(relation: Relation): Observable<Relation[]> {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
-    return this.http.put(`${this.URLBase}/character/${relation.requester['id']}/relate/${relation.requested['id']}`, relation, options)
+    return this.http.put(`${this.URLBase}/relation/${relation.requester['id']}/related/${relation.requested['id']}`, relation, options)
                     .map((res: Response) => res.json())
                     .catch((error: any) => Observable.throw(error.json().error) || 'Server error');
   }
@@ -27,7 +27,7 @@ export class RelationService {
   deleteRelation(relation: Relation): Observable<Relation[]> {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
-    return this.http.delete(`${this.URLBase}/character/${relation.requester['id']}/relate/${relation.requested['id']}`)
+    return this.http.delete(`${this.URLBase}/relation/${relation.requester['id']}/related/${relation.requested['id']}`)
                     .map((res: Response) => res.json())
                     .catch((error: any) => Observable.throw(error.json().error) || 'Server error');
   }
